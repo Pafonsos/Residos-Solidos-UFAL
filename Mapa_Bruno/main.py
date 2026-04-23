c34c8ea804e58d20fd6d8f4d21ea2e93a7e5c16d
Pedro Afonso Santana Santos <afonsoaq38@gmail.com>
2026-04-22 22:09:37 -0300

    mapa

--- Mapa_Bruno/main.py
+++ Mapa_Bruno/main.py
@@ -1,9 +1,13 @@
 import geopandas as gpd
 import dash
 from dash import html
+#Para rodar o código, é necessário instalar as bibliotecas geopandas e dash utilizando o pip:
+#pip install geopandas dash
+#em rode: pip install 'folium>=0.12.0' para evitar erros de compatibilidade.
+#em seguida no terminal rode: python Mapa_Bruno/main.py
 
 #READ FILE WITH GEOPANDAS AND SHOW HEADS
-bairros = gpd.read_file("Mapa_Bruno/AL_bairros_CD2022.dbf")
+bairros = gpd.read_file("Mapa_Bruno/AL_bairros_CD2022/AL_bairros_CD2022.shp")
 bairros.head()
 
 
@@ -11,6 +15,7 @@ bairros.head()
 filter_maceio = bairros.loc[bairros['NM_MUN'] == 'Maceió']
 filter_maceio = filter_maceio.drop(['CD_REGIAO', 'NM_REGIAO', 'CD_UF', 'CD_DIST', 'NM_DIST', 'CD_SUBDIST', 'CD_BAIRRO', 'CD_RGINT', 'CD_RGI', 'NM_RGI', 'CD_CONCURB', 'NM_CONCURB'], axis=1)
 new_bairros = filter_maceio.explore(tiles="CartoDB positron", min_zoom=5, max_zoom=20,)
+new_bairros.save("Mapa_Bruno/mapa_bairros_maceio.html")
 mapa_html = new_bairros._repr_html_()
 
 
