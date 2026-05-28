649ebf823b82db145aafb449bdd06c42cb9f2a38
Pedro Afonso Santana Santos <afonsoaq38@gmail.com>
2026-05-27 21:21:37 -0300

    2

--- layout.py
+++ layout.py
@@ -4,19 +4,40 @@ layout = html.Div([
     html.Div([
 
         html.Div([
+            html.Div([
 
             html.Div([
+                html.Span('Visualização:', className="Texto-dropdown"),
                 html.Button('Diário', className="botão-visualização"),
                 html.Button('Semanal', className="botão-visualização"),
                 html.Button('Mensal', className="botão-visualização"),
-            ], className="card-filtros-conteudo-esquerda"),
+            ], className="card-filtros-conteudo-cima"),
+
+            html.Div([
+                html.Span('Periodo:', className="Texto-dropdown"),
+                dcc.Input(type='text', className="input-pesquisa",id='inicial'),
+                dcc.Input(type='text', className="input-pesquisa",id='final')
+            ], className="card-filtros-conteudo-baixo"),]
+            ,className="card-filtros-conteudo-esquerda"),
 
             html.Div([
                 html.Button('Bairro', className="botão"),
                 html.Button('Região Administrativa', className="botão"),
+                html.Span('Selecionar:', className="Texto-dropdown"),
+                dcc.Dropdown(
+                    options=[
+                        {'label': 'Maceio', 'value': 'xxxxxxxxxxx'},
+                        {'label': 'biu', 'value': 'xxxxxxxxxx'},
+                    ],
+                    value='xxxxxxxxxxx',
+                    clearable=False,
+                    searchable=False,
+                    className="dropdown"
+                )
             ], className="card-filtros-conteudo-centro"),
 
             html.Div([
+                html.Span('Escopo:', className="Texto-dropdown"),
                 dcc.Dropdown(
                     options=[
                         {'label': 'grafico1', 'value': 'xxxxxxxxxxx'},
